5736c1ca8b71e106d5a1d0a8d8ab73c4c4312c30
darkshadow4568 <66343444+darkshadow4568@users.noreply.github.com>
2022-03-31 16:47:43 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/WebAR0331.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Web.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
